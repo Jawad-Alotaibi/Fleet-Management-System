98dabc21b51038ac6a0944f00dcab179577080b6
Jawad Alotaibi <jawadk.alotaibi@gmail.com>
2026-09-13 11:40:55 +0300

    Comment on hte interface

--- FleetManagementSystem/Interfaces/IMaintainable.cs
+++ FleetManagementSystem/Interfaces/IMaintainable.cs
@@ -1,6 +1,6 @@
 namespace FleetManagementSystem.Interfaces;
 
-public interface IMaintainable
+public interface IMaintainable // a contract for any maintainable object
 {
     // Task 6.1 – Define PerformMaintenance method
       void PerformMaintenance();
--- FleetManagementSystem/obj/Debug/net10.0/FleetManagementSystem.AssemblyInfo.cs
+++ FleetManagementSystem/obj/Debug/net10.0/FleetManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d2e236b1f7a990d41542f19927050a6c208d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8499d9d0d6555a5ce382a9c92ad28e39f4fc5750")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
